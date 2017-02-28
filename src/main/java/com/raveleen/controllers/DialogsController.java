919779cb5d2c70aaff8842e825854756dd18817c
Raveleen <svjatoslavkrit@gmail.com>
2017-02-28 15:14:33 +0200

    code conv. edit

--- src/main/java/com/raveleen/controllers/DialogsController.java
+++ src/main/java/com/raveleen/controllers/DialogsController.java
@@ -1,8 +1,5 @@
 package com.raveleen.controllers;
 
-import java.io.IOException;
-import java.util.List;
-
 import com.raveleen.entities.CustomUser;
 import com.raveleen.entities.Dialog;
 import com.raveleen.entities.Message;
@@ -10,6 +7,10 @@ import com.raveleen.services.DialogService;
 import com.raveleen.services.MessageService;
 import com.raveleen.services.UserService;
 import com.raveleen.services.UtilsService;
+
+import java.io.IOException;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
@@ -56,11 +57,13 @@ public class DialogsController {
         CustomUser customUserSecond = userService.getUserById(id);
 
         if (dialogService.isDialogExists(customUser.getId(), customUserSecond.getId())) {
-            System.out.println("dialog " + customUser.getId() + customUserSecond.getId() + "exists(true)");
+            System.out.println("dialog " + customUser.getId()
+                    + customUserSecond.getId() + "exists(true)");
             Dialog temp = dialogService.getDialog(customUser.getId(), customUserSecond.getId());
             return "redirect:/dialog/" + temp.getId();
         } else {
-            System.out.println("dialog " + customUser.getId() + customUserSecond.getId() + "don't exists(false)");
+            System.out.println("dialog " + customUser.getId()
+                    + customUserSecond.getId() + "don't exists(false)");
             Dialog dialog = new Dialog(customUser, customUserSecond);
             dialogService.addDialog(dialog);
             Dialog temp = dialogService.getDialog(customUser.getId(), customUserSecond.getId());
@@ -79,15 +82,17 @@ public class DialogsController {
         System.out.println(dialog);
         CustomUser us1 = dialog.getUser1();
         CustomUser us2 = dialog.getUser2();
+        long us1ID = us1.getId();
+        long us2ID = us2.getId();
 
-        if (us1.getId() == dbUser.getId()) {
+        if (us1ID == dbUser.getId()) {
             model.addAttribute("dialogUser", us2);
-            model.addAttribute("dialogUserFollowers", userService.getNumberOfFollowers(us2.getId()));
-            model.addAttribute("dialogUserFollowing", userService.getNumberOfFollowings(us2.getId()));
+            model.addAttribute("dialogUserFollowers", userService.getNumberOfFollowers(us2ID));
+            model.addAttribute("dialogUserFollowing", userService.getNumberOfFollowings(us2ID));
         } else {
             model.addAttribute("dialogUser", us1);
-            model.addAttribute("dialogUserFollowers", userService.getNumberOfFollowers(us1.getId()));
-            model.addAttribute("dialogUserFollowing", userService.getNumberOfFollowings(us1.getId()));
+            model.addAttribute("dialogUserFollowers", userService.getNumberOfFollowers(us1ID));
+            model.addAttribute("dialogUserFollowing", userService.getNumberOfFollowings(us1ID));
         }
         model.addAttribute("dialog", dialog);
         model.addAttribute("user", dbUser);
@@ -106,18 +111,18 @@ public class DialogsController {
         List<Dialog> dialogs = dialogService.dialogsOrderByDate(customUser.getId(), from);
         String[] response = new String[dialogs.size()];
 
-        int i = 0;
+        int counter = 0;
         for (Dialog temp : dialogs) {
             CustomUser us1 = temp.getUser1();
             CustomUser us2 = temp.getUser2();
 
             if (us1.getId() == customUser.getId()) {
-                response[i] = utilsService.createFragmentDialog(temp, customUser, us2);
+                response[counter] = utilsService.createFragmentDialog(temp, customUser, us2);
             } else {
-                response[i] = utilsService.createFragmentDialog(temp, customUser, us1);
+                response[counter] = utilsService.createFragmentDialog(temp, customUser, us1);
             }
 
-            i++;
+            counter++;
         }
 
         return response;
@@ -125,7 +130,8 @@ public class DialogsController {
 
     @RequestMapping(value = "/messages/{dialog-id}/{from}")
     @ResponseBody
-    public String[] getMessagesList(@PathVariable("dialog-id") long dialogId,@PathVariable("from") int from) {
+    public String[] getMessagesList(@PathVariable("dialog-id") long dialogId,
+                                    @PathVariable("from") int from) {
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         String login = user.getUsername();
         CustomUser customUser = userService.getUserByLogin(login);
@@ -134,11 +140,11 @@ public class DialogsController {
         String[] response = new String[messages.size()];
 
         System.out.println(from);
-        int i = 0;
+        int counter = 0;
         for (Message message : messages) {
             System.out.println(message);
-            response[i] = utilsService.createFragmentMessage(message, customUser);
-            i++;
+            response[counter] = utilsService.createFragmentMessage(message, customUser);
+            counter++;
         }
 
         return response;
@@ -146,7 +152,8 @@ public class DialogsController {
 
     @RequestMapping(value = "/messages-get-unread/{dialog-id}/{time}")
     @ResponseBody
-    public String[] getMessagesList(@PathVariable("dialog-id") long dialogId,@PathVariable("time") long time) {
+    public String[] getMessagesList(@PathVariable("dialog-id") long dialogId,
+                                    @PathVariable("time") long time) {
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         String login = user.getUsername();
         CustomUser customUser = userService.getUserByLogin(login);
@@ -157,10 +164,10 @@ public class DialogsController {
         String[] response = new String[messages.size() + 1];
         response[0] = "" + dialog.getLastMessageDate().getTime();
 
-        int i = 1;
+        int counter = 1;
         for (Message message : messages) {
-            response[i] = utilsService.createFragmentMessage(message, customUser);
-            i++;
+            response[counter] = utilsService.createFragmentMessage(message, customUser);
+            counter++;
         }
 
         return response;
@@ -168,19 +175,20 @@ public class DialogsController {
 
     @RequestMapping(value = "/message-create/{dialog-id}", method = RequestMethod.POST)
     @ResponseBody
-    public String createMessage(@RequestParam("message-text") String text, @PathVariable("dialog-id") long dialogId) throws IOException {
+    public String createMessage(@RequestParam("message-text") String text,
+                                @PathVariable("dialog-id") long dialogId) throws IOException {
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         String login = user.getUsername();
         CustomUser customUser = userService.getUserByLogin(login);
 
         Dialog dialog = dialogService.getById(dialogId);
-            System.out.println(dialog);
+        System.out.println(dialog);
         Message message = new Message();
         message.setFrom(customUser);
         message.setText(text);
         message.setIsread(false);
         message.setDialog(dialog);
-            System.out.println(message);
+        System.out.println(message);
         Message temp = messageService.addMessage(message);
         System.out.println(temp + " " + temp.getCreateDate());
         dialog.setLastMessageDate(temp.getCreateDate());
