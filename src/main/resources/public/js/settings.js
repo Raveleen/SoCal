ece7157422377bbc0fb91bfb11a3bec4d8a89970
Raveleen <svjatoslavkrit@gmail.com>
2017-03-02 22:52:31 +0200

    code duplication refactoring

--- src/main/resources/public/js/settings.js
+++ src/main/resources/public/js/settings.js
@@ -15,6 +15,41 @@ $(document).ready(function () {
             if (log) alert(log);
         }
     }
+    function disableUpdate(temp) {
+        $("#update").prop("disabled", true);
+        $(temp).show();
+    }
+    function enableUpdate() {
+        if ((input_a + input_b + input_c) === 3) {
+            $("#update").prop("disabled", false);
+        }
+    }
+    function hideAndRemoveHidden(elementsIDs) {
+        for (var i = 0; i < elementsIDs.length; i++) {
+            $(elementsIDs[i]).hide();
+            $(elementsIDs[i]).removeClass("hidden");
+        }
+    }
+    function inputCheck(inputId, size, alert, varName) {
+        hideAndRemoveHidden([alert]);
+        if ($(inputId).val().length < size) {
+            if (varName === "c") {
+                input_c = 0;
+            }
+            if (varName === "b") {
+                input_b = 0;
+            }
+            signUpDisableToTrueAndShowElement(alert);
+        } else {
+            if (varName === "c") {
+                input_c = 1;
+            }
+            if (varName === "b") {
+                input_b = 1;
+            }
+            signUpDisableToFalse();
+        }
+    }
     $("#upload").prop("disabled", true);
     document.getElementById("info").defaultValue = $("#usinfo").text();
     var input_a = 1;
@@ -23,70 +58,35 @@ $(document).ready(function () {
     $("#update").prop("disabled", false);
     $("#login-input").keyup(function () {
         $.get("settings/validation?login=" + $("#login-input").val(), function (data) {
-            $("#alert-login-too-short").hide();
-            $("#alert-login-is-not-valid").hide();
-            $("#alert-login-is-not-a-word").hide();
-            $("#alert-login-is-valid").hide();
-            $("#alert-login-too-short").removeClass("hidden");
-            $("#alert-login-is-not-valid").removeClass("hidden");
-            $("#alert-login-is-not-a-word").removeClass("hidden");
-            $("#alert-login-is-valid").removeClass("hidden");
+            hideAndRemoveHidden(["#alert-login-too-short","#alert-login-is-not-valid",
+                "#alert-login-is-not-a-word","#alert-login-is-valid"]);
             if (data === "1") {
                 input_a = 0;
-                $("#update").prop("disabled", true);
-                $("#alert-login-too-short").show();
+                disableUpdate("#alert-login-too-short");
             }
             if (data === "2") {
                 input_a = 1;
-                if ((input_a + input_b + input_c) == 3) {
-                    $("#update").prop("disabled", false);
-                }
+                enableUpdate()
             }
             if (data === "3") {
                 input_a = 0;
-                $("#update").prop("disabled", true);
-                $("#alert-login-is-not-valid").show();
+                disableUpdate("#alert-login-is-not-valid");
             }
             if (data === "4") {
                 input_a = 0;
-                $("#update").prop("disabled", true);
-                $("#alert-login-is-not-a-word").show();
+                disableUpdate("#alert-login-is-not-a-word");
             }
             if (data === "5") {
                 input_a = 1;
-                if ((input_a + input_b + input_c) === 3) {
-                    $("#update").prop("disabled", false);
-                }
+                enableUpdate();
             }
         })
     });
     $("#email-input").keyup(function () {
-        $("#alert-email-too-short").hide();
-        $("#alert-email-too-short").removeClass("hidden");
-        if ($("#email-input").val().length < 5) {
-            input_b = 0;
-            $("#update").prop("disabled", true);
-            $("#alert-email-too-short").show();
-        } else {
-            input_b = 1;
-            if ((input_a + input_b + input_c) == 3) {
-                $("#update").prop("disabled", false);
-            }
-        }
+        inputCheck("#email-input", 5, "#alert-email-too-short", "b");
     });
     $("#phone-input").keyup(function () {
-        $("#alert-phone-too-short").hide();
-        $("#alert-phone-too-short").removeClass("hidden");
-        if ($("#phone-input").val().length < 8) {
-            input_c = 0;
-            $("#update").prop("disabled", true);
-            $("#alert-phone-too-short").show();
-        } else {
-            input_c = 1;
-            if ((input_a + input_b + input_c) == 3) {
-                $("#update").prop("disabled", false);
-            }
-        }
+        inputCheck("#phone-input", 8, "#alert-phone-too-short", "c");
     });
     $("#info").keyup(function () {
         $("#characters-number").text(200 - $("#info").val().length);
@@ -95,7 +95,7 @@ $(document).ready(function () {
         var input = $(this).parents('.input-group').find(':text'),
             log = numFiles > 1 ? numFiles + ' files selected' : label;
 
-        if (numFiles == 0) {
+        if (numFiles === 0) {
             inputLog(input, log);
             $("#upload").prop("disabled", true);
         } else {
