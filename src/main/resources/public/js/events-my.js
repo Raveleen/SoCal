4b332508022f91111d74cf13879e56df5dfec4d7
Raveleen <svjatoslavkrit@gmail.com>
2017-03-15 12:37:00 +0200

    events attachment

--- src/main/resources/public/js/events-my.js
+++ src/main/resources/public/js/events-my.js
@@ -6,10 +6,11 @@ $(document).ready(function () {
     var user_id = $("#ids").text();
     var from = 0;
     var flag = true;
-    var special_alert_no_my_future_events = "<div id=\"special-alert-no-my-future-events\" class=\"appended-result\"><div class=\"row search-result\"><div class=\"col-sm-12\"><div id=\"no-more-posts\"><h5>THERE IS NO POSTS.</h5></div></div></div><hr class=\"middle\"></div>";
-    var array = getFirstTen(user_id, "/get-my-future-events/", "#special-alert-no-my-future-events", "#my-events", load_more_my_future_events );
+    var special_alert_no_my_future_events = "<div id=\"special-alert-no-my-future-events\" class=\"appended-result\"><div class=\"row search-result\"><div class=\"col-sm-12\"><div class=\"no-more-events\"><h5>THERE IS NO EVENTS.</h5></div></div></div><hr class=\"middle\"></div>";
+    $("#my-events").append(special_alert_no_my_future_events);
+    var array = getFirstTen(user_id, "/get-my-future-events/", "#special-alert-no-my-future-events", "#my-events", load_more_my_future_events);
     $(document.body).on("click", "#load-more-my-future-events-href", function () {
-        var array = getNextTen(user_id, );
+        var array = getNextTen(user_id, from, "/get-my-future-events/", "#my-events", load_more_my_future_events);
         from = array[0];
     });
 });
